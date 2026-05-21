f5278fd25f7630027bc82bb16ca2f0463faec5f3
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2026-05-21 08:25:41 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.composeHotReload) apply false
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(ktorLibs.plugins.ktor) apply false
